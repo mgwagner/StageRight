74f7c089c7e757017b2949acc5f8a1ba09bbb119
Sterling Conner <saconner@smu.edu>
2018-04-07 15:35:32 -0500

    Add files via upload
    Pushing to new branch

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   resources :users
   get 'plays/index'
   post "plays/upload"
-  post 'plays/cuesDB'
   get 'login/index'
-  root to: 'login#index'
+  root to: 'plays#index'
   
   mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402230359) do
+ActiveRecord::Schema.define(version: 20180407075634) do
 
   create_table "notes", force: :cascade do |t|
     t.integer "script_id"
@@ -18,8 +18,6 @@ ActiveRecord::Schema.define(version: 20180402230359) do
     t.text "note_content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "lineNum"
-    t.integer "location"
   end
 
   create_table "scripts", force: :cascade do |t|
@@ -27,7 +25,6 @@ ActiveRecord::Schema.define(version: 20180402230359) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "filename"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,6 +36,11 @@ ActiveRecord::Schema.define(version: 20180402230359) do
     t.text "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end

4c7755f1c7e1fcc6847d60da8e0e7c129c4f9ece
Matthew Wagner <MatthewGWagner@Matthews-MacBook-Pro-3.local>
2018-04-27 17:17:05 -0500

    fixed adding cues and viewing them on page

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -43,25 +43,31 @@ class PlaysController < ApplicationController
   end
 
   def cuesDB
-    puts "something here so that we can see its actually doing something"
     dbScript = Script.where(user_id: 1)
     if (dbScript.empty?)
       render json: {"Failure" => "'You have no arms or legs!' 'Its just a flesh wound'"}
-    else
-      cuesLink = Note.new(
-        user_id: 1, 
-        script_id: dbScript[0].id,
-        cueType: params[:cueType], 
-        cueLabel: params[:cueLabel], 
-        cueDescription: params[:cueDescription], 
-        location: params[:location], 
-        lineNum: params[:cueLineNum])
-      
-      if (cuesLink.save)
-        render json: cuesLink
-      else
-        render json: {"Failure" => "Failure...JK! Success! wait...no....due to budget cuts you are indeed a....Success! No, FAILURE!!!"}
-      end
+    else 
+      cues = []
+      Note.where(user_id: 1, script_id: dbScript[0].id, location: params[:location], lineNum: params[:cueLineNum]).destroy_all
+      for i in 0...params[:cueType].length do
+        if (params[:cueType][params[:cueType].length-1-i] != "" ||  params[:cueLabel][params[:cueLabel].length-1-i] != "" || params[:cueDescription][params[:cueDescription].length-1-i] != "")
+          cuesLink = Note.new(
+          user_id: 1, 
+          script_id: dbScript[0].id,
+          cueType: params[:cueType][params[:cueType].length-1-i], 
+          cueLabel: params[:cueLabel][params[:cueLabel].length-1-i], 
+          cueDescription: params[:cueDescription][params[:cueDescription].length-1-i], 
+          location: params[:location], 
+          lineNum: params[:cueLineNum])
+        end
+        if (cuesLink.save)
+          cues.push(cuesLink)
+        else
+          render json: {"Failure" => "Failure...JK! Success! wait...no....due to budget cuts you are indeed a....Success! No, FAILURE!!!"}
+          return
+        end
+      end 
+      render json: cues
     end
   end
 end
